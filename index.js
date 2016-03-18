dadf9a91d099a75e9d272ebbb2ea841c39c6f781
Geraint White <geraintwhite@gmail.com>
2016-03-18 20:35:36 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var do_wrapper = require('do-wrapper');
+var config = require('./config');
+
+var dig = new do_wrapper(config.api_key);
+var log = function (err, res, body) { console.log(err || body); };
+
+
+function get_snapshot_id (cb) {
+	dig.imagesGetAll({private:true}, function (err, res, body) {
+		if (err) throw err;
+		cb(body.images.filter(function (image) {
+			return (new RegExp(config.droplet.name+'-[0-9]+')).test(image.name);
+		}).map(function (image) {
+			return image.id;
+		})[0]);
+	});
+}
+
+function create_from_snapshot (cb) {
+	get_snapshot_id(function (id) {
+		config.droplet.image = id;
+		dig.dropletsCreate(config.droplet, function (err, res, body) {
+			if (err) throw err;
+			cb(body.id);
+		});
+	});
+}
+
