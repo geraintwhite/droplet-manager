186918ad358f850a5fdfe1b3166a11fa89cfc61c
Geraint White <geraintwhite@gmail.com>
2016-04-13 16:05:34 +0100

    Add some more droplet functions

--- index.js
+++ index.js
@@ -6,23 +6,46 @@ var dig = new do_wrapper(config.api_key);
 var out = {};
 
 out.get_snapshot_id = function(cb) {
-  dig.imagesGetAll({private:true}, function (err, res, body) {
-    if (err) throw err;
-    cb(body.images.filter(function (image) {
+  dig.imagesGetAll({private: true}, function(err, res, body) {
+    cb(body.images.filter(function(image) {
       return (new RegExp(config.droplet.name+'-[0-9]+')).test(image.name);
-    }).map(function (image) {
+    }).map(function(image) {
       return image.id;
     })[0]);
   });
 };
 
 out.create_from_snapshot = function(cb) {
-  get_snapshot_id(function (id) {
+  out.get_snapshot_id(function(id) {
     config.droplet.image = id;
-    dig.dropletsCreate(config.droplet, function (err, res, body) {
-      if (err) throw err;
-      cb(body.id);
+    dig.dropletsCreate(config.droplet, function(err, res, body) {
+      if (res.statusCode === 202) {
+        config.droplet.id = body.droplet.id;
+        cb(body.droplet.id);
+      }
     });
   });
 };
 
+out.power_off_droplet = function(id, cb) {
+  dig.dropletsRequestAction(id, {type: 'power_off'}, function(err, res, body) {
+    cb(body.action.id);
+  });
+};
+
+out.snapshot_droplet = function(id, cb) {
+  dig.dropletsRequestAction(id, {
+    type: 'snapshot',
+    name: config.droplet.name + '-' + Date.now()
+  }, function(err, res, body) {
+    cb(body.action.id);
+  });
+};
+
+out.destroy_droplet = function(id, cb) {
+  dig.dropletsDelete(id, function(err, res, body) {
+    cb(res.statusCode === 204);
+  });
+};
+
+module.exports = out;
