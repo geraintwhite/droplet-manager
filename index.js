b55e60bf1a1daa9b0645276db01325e47c5f38cb
Geraint White <geraintwhite@gmail.com>
2016-04-13 15:04:43 +0100

    Reindent

--- index.js
+++ index.js
@@ -3,25 +3,26 @@ var config = require('./config');
 
 var dig = new do_wrapper(config.api_key);
 
+var out = {};
 
-function get_snapshot_id (cb) {
-	dig.imagesGetAll({private:true}, function (err, res, body) {
-		if (err) throw err;
-		cb(body.images.filter(function (image) {
-			return (new RegExp(config.droplet.name+'-[0-9]+')).test(image.name);
-		}).map(function (image) {
-			return image.id;
-		})[0]);
-	});
-}
+out.get_snapshot_id = function(cb) {
+  dig.imagesGetAll({private:true}, function (err, res, body) {
+    if (err) throw err;
+    cb(body.images.filter(function (image) {
+      return (new RegExp(config.droplet.name+'-[0-9]+')).test(image.name);
+    }).map(function (image) {
+      return image.id;
+    })[0]);
+  });
+};
 
-function create_from_snapshot (cb) {
-	get_snapshot_id(function (id) {
-		config.droplet.image = id;
-		dig.dropletsCreate(config.droplet, function (err, res, body) {
-			if (err) throw err;
-			cb(body.id);
-		});
-	});
-}
+out.create_from_snapshot = function(cb) {
+  get_snapshot_id(function (id) {
+    config.droplet.image = id;
+    dig.dropletsCreate(config.droplet, function (err, res, body) {
+      if (err) throw err;
+      cb(body.id);
+    });
+  });
+};
 
